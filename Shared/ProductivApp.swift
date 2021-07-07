09eac70f9f6bf637a98363806e1b4b45d010bae0
Dev Patel <devpatel05@gmaill.com>
2021-07-06 21:52:11 -0500

    Initial Commit

---
+++ Shared/ProductivApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivApp.swift
+//  Shared
+//
+//  Created by Dev Patel on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ProductivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
