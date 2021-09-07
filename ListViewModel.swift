f25ac8b9774320952f9326f00c9e5436c6d17024
Dev Patel <devpatel05@gmaill.com>
2021-09-06 22:28:57 -0500

    to-do frontEnd

---
+++ ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  
+//
+//  Created by Dev Patel on 7/20/21.
+//
+
+import Foundation
---
+++ TaskCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskCellViewModel.swift
+//  
+//
+//  Created by Dev Patel on 7/20/21.
+//
+
+import Foundation
